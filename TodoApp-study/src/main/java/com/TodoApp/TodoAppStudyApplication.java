ff367e7081b6b54f4ef6953446fbc0b22c3017a3
dakyungYI <leeda407@gmail.com>
2023-08-07 20:24:45 +0900

    To-do Application 과제 입니다.

---
+++ TodoApp-study/src/main/java/com/TodoApp/TodoAppStudyApplication.java
@@ -0,0 +1,13 @@
+package com.TodoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppStudyApplication.class, args);
+	}
+
+}
---
+++ TodoApp-study/src/test/java/com/TodoApp/TodoAppStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TodoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
